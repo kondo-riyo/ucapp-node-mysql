cc9de90e62febbfde061348ee3ca704dacbdee1c
kondo-riyo <r1y0.tenten.1126@gmail.com>
2021-12-06 16:16:58 +0900

    mysqlデータベースへの追加

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mysql = require('mysql');
+// const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use('/img', express.static(__dirname + '/dist/img/'));
 app.use('/css', express.static(__dirname + '/dist/css/'));
 app.use('/js', express.static(__dirname + '/dist/js/'));
@@ -24,6 +27,8 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
         }
         console.log('success');
     });
+
+    //usersテーブルを全て取得-----------------------------------------
     app.get('/api/users', (req, res) => {
         connection.query('SELECT * FROM users',
             (error, results) => {
@@ -32,5 +37,24 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
             }
         );
     });
+
+    const jsonWrite = function(res, ret) {
+        if(typeof ret === 'undefined') {
+            res.json({
+                code: '1',
+                msg: '操作失败'
+            });
+        } else {
+            res.json(ret);
+        }
+    };
+    //usersテーブルに追加---------------------------------------
+    app.post('/api/signIn', (req, res) => {
+        const params = req.body;
+        const sqlInsert= `INSERT INTO users VALUES (?,?,?,?)`
+        connection.query(sqlInsert, [params.userId, params.userName, params.mail, params.password], (err,result)=>{
+            res.send(result);
+    });
+    })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
